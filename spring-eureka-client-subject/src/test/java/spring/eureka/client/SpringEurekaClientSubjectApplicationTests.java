53d57ef671daefca4cdfb3928c4d89a18b3c2db8
vikastaank <vikas.taank@gmail.com>
2017-05-18 20:59:19 +0530

    Committing Spring Cloud Files

---
+++ spring-eureka-client-subject/src/test/java/spring/eureka/client/SpringEurekaClientSubjectApplicationTests.java
@@ -0,0 +1,16 @@
+package spring.eureka.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringEurekaClientSubjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
